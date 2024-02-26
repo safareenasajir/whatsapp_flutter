49895e92a82416ff1f6dc42ac2e239cf451620b4
safareenasajir <safareenasafa13@gmail.com>
2024-02-26 11:31:13 +0530

    first

--- lib/whatsapp.dart
+++ lib/whatsapp.dart
@@ -5,12 +5,13 @@ class Whatsapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //scaffold to get the screen.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: const Text("WhatsApp"),
         foregroundColor: Colors.white,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined)),
+        actions: [IconButton(onPressed: () {}, icon:  const Icon(Icons.camera_alt_outlined)),
         IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
         IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
 
@@ -20,32 +21,71 @@ class Whatsapp extends StatelessWidget {
       body:
       
       Container(
-        height: 50,
+        height: 200,
         width: double.infinity,
         margin: const EdgeInsets.all(20),
-        child: const 
-           Row(
-            children: [
-            
-            CircleAvatar(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text("Safareena"),
-                Text("hello")
-              ],
-            ),
-            Spacer(),
-            Text("10.00am"),
-        
-            
-            
-           
-            
-          
-          ]),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+                children: [
+                
+                CircleAvatar(),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Safareena"),
+                    Text("hello"),
+                    
+                  ],
+                ),
+                Spacer(),
+                Text("10.00am"),
+                  
+                
+                
+               
+                
+              
+              ]),
+              Row(
+                children: [
+                  
+                  CircleAvatar(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      
+                      Text("Jaseela"),
+                      Text("hi"),
+                    ],
+                  ),
+                  Spacer(),
+                  Text("12.00am"),
+                  
+                  
+                ],
+                
+              ),
+              Row(
+                children: [
+                  CircleAvatar(),
+                  Column(
+                    children: [
+                      Text("Yasmin"),
+                      Text("good morning"),
+                    ],
+                  ),
+                  Spacer(),
+                  Text("11.00am"),
+
+                ],
+              )
+          ],
+        ),
           
       ),
+      
       
 
       
